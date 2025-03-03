34b954335a38d4e511c61896e40340e5092a6389
Gabriel-Jacquinot <gabejacquinot0@gmail.com>
2025-03-03 11:28:42 +0000

    Value changes

--- menu/menu.py
+++ menu/menu.py
@@ -3,9 +3,9 @@ from .buttons import Button
 
 def create_menu(window, WIDTH, HEIGHT):
     # Buttons which can be made and their properties
-    play_computer = Button("Calbri", 60, False, "Play the Computer", True, (50, 50, 50), "white", ((WIDTH/ 2) - (400/ 2)), ((HEIGHT/ 2) - (190/ 2)) - 33, 395, 60, "white", hoverColour = (0, 0, 0)) # change the last two numbers for the button length and width and the two numbers before for x, y position from the top left corner
-    play_local = Button("Calbri", 60, False, "Play a Friend", True, (50, 50, 50), "white", ((WIDTH/ 2) - (285/ 2)), ((HEIGHT/ 2) - (60/ 2)) - 30, 285, 60, "white", hoverColour = (0, 0, 0)) # change the last two numbers for the button length and width and the two numbers before for x, y position from the top left corner
-    quit_button = Button("Calbri", 60, False, "Quit", True, (50, 50, 50), "white", ((WIDTH/ 2) - (112/ 2)), ((HEIGHT/ 2) + (75/ 2)) - 27, 112, 60, "red", hoverColour = (0, 0, 0))
+    play_computer = Button("Calbri", 60, False, "Play vs Computer", True, (50, 50, 50), "white", ((WIDTH/ 2) - (380/ 2)), ((HEIGHT/ 2) - (190/ 2)) - 33, 380, 60, "white", hoverColour = (0, 0, 0)) # change the last two numbers for the button length and width and the two numbers before for x, y position from the top left corner
+    play_local = Button("Calbri", 60, False, "Play vs Friend", True, (50, 50, 50), "white", ((WIDTH/ 2) - (310/ 2)), ((HEIGHT/ 2) - (60/ 2)) - 30, 310, 60, "white", hoverColour = (0, 0, 0)) # change the last two numbers for the button length and width and the two numbers before for x, y position from the top left corner
+    quit_button = Button("Calbri", 60, False, "Quit", True, (50, 50, 50), "white", ((WIDTH/ 2) - (112/ 2)), ((HEIGHT/ 2) + (75/ 2)) - 28, 112, 60, "red", hoverColour = (0, 0, 0))
     
     # Making the buttons
     quit_button.make_button(window) # Actually making the button now and putting it on the window
